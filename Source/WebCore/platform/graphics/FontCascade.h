7ad5ea3859a02433386a6bb0e1419e0258ccc6f8
Driftstack <dev@driftstack.dev>
2026-05-13 23:21:40 +0200

    V-790.N FontCascade.h displayListForGlyphBuffer StringView source param (wave 29-156)

--- Source/WebCore/platform/graphics/FontCascade.h
+++ Source/WebCore/platform/graphics/FontCascade.h
@@ -202,7 +202,7 @@ public:
     static constexpr float syntheticObliqueAngle() { return 14; }
 
     RefPtr<const DisplayList::DisplayList> displayListForTextRun(GraphicsContext&, const TextRun&, unsigned from = 0, std::optional<unsigned> to = { }, CustomFontNotReadyAction = CustomFontNotReadyAction::DoNotPaintIfFontNotReady) const;
-    RefPtr<const DisplayList::DisplayList> displayListForGlyphBuffer(GraphicsContext&, const GlyphBuffer&, CustomFontNotReadyAction) const;
+    RefPtr<const DisplayList::DisplayList> displayListForGlyphBuffer(GraphicsContext&, const GlyphBuffer&, CustomFontNotReadyAction, StringView source = { }) const;
 
     unsigned generation() const { return m_generation; }
 
