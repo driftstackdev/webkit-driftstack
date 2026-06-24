8952f25854bdbab5fd996b05d0529def5f8c026a
Driftstack <dev@driftstack.dev>
2026-06-24 09:54:16 -0400

    Revert "build: export EmptyGamepadProvider (Private header + WEBCORE_EXPORT) for A3 W2875"
    This reverts commit 852addfac04e5c4dcbb31140b2781e5970d72a38.

--- Source/WebCore/platform/gamepad/EmptyGamepadProvider.h
+++ Source/WebCore/platform/gamepad/EmptyGamepadProvider.h
@@ -31,7 +31,7 @@
 
 namespace WebCore {
 
-class WEBCORE_EXPORT EmptyGamepadProvider final : public GamepadProvider {
+class EmptyGamepadProvider final : public GamepadProvider {
 public:
     ~EmptyGamepadProvider() final { }
 
